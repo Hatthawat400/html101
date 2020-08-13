708f1d595ed5997b86c71771e745ca7009009390
Hatthawat400 <commy2812@gmail.com>
2020-08-13 16:03:29 +0700

    hello

---
+++ my-project313/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="row">
+  <div class="col-3"><Counter lable="ok"/></div>
+  <div class="col-3"><Counter lable="ชอบ"/></div>
+  <div class="col-3"><Counter lable="รัก"/></div>
+  <div class="col-3"><Counter lable="เกลียด"/></div>
+</div>
+  
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Counter, 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-project313/src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="example-1">
+    <button v-on:click="up" class="btn btn-danger">{{lable}}</button>
+
+    <p v-bind;style="{fontsize:fsize+'px'}">{{ counter }} times.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+      fsize: 50,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    up: function () {
+      counter += 1;
+      fsize += 10;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
